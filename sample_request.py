32b3f48c2c3e0481fa23fe2dd4f4727bbe962fcd
Hailu <hailutejut@gmail.com>
2022-01-20 13:16:43 -0500

    Added only a line breaker to test CI with GitHub Actions.

--- sample_request.py
+++ sample_request.py
@@ -7,7 +7,8 @@ scores = os.system("python starter/model_performance_on_slices.py")
 data = scores
 
 r = requests.post(
-    "http://127.0.0.1:8000/scores/", data=json.dumps(data)
+    "http://127.0.0.1:8000/scores/",
+    data=json.dumps(data)
 )
 
 print(r.json())
