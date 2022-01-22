0c68187b41e69d3bbcccea3b622d570ac7d6d59a
Hailu <hailutejut@gmail.com>
2022-01-22 09:00:16 -0500

    Update: remove the try-except block in test_data.py

--- test_data.py
+++ test_data.py
@@ -9,15 +9,16 @@ import dvc.api
 @pytest.fixture(scope="session")
 def data():
     """ A function to read the cleaned version of the dataset."""
-    try:
-        df = pd.read_csv("data/census_clean.csv")
-    except:
-        census_data_clean = dvc.api.read(
-            '"data/census_clean.csv"',
-            repo='https://github.com/hailuteju/deploying-ml-model'
-        )
-        census_data_csv = io.StringIO(census_data_clean)
-        df = pd.read_csv(census_data_csv)
+    # try:
+    #     df = pd.read_csv("data/census_clean.csv",
+    #                      encoding='utf-8')
+    # except:
+    census_data_clean = dvc.api.read(
+        '"data/census_clean.csv"',
+        repo='https://github.com/hailuteju/deploying-ml-model'
+    )
+    census_data_csv = io.StringIO(census_data_clean)
+    df = pd.read_csv(census_data_csv)
 
     return df
 
